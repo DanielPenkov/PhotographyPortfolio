76ed209fe3e8f4b1996014ff51e853e56002c491
danielpenkov <penkov.daniel@gmail.com>
2016-04-30 11:35:33 +0200

    Clear

--- plugins/Admin/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
-
-Router::plugin('Admin', function (RouteBuilder $routes) {
-    $routes->connect('/', ['plugin' => 'Admin', 'controller' => 'pictures', 'action' => 'index']);
-    $routes->extensions(['json']);
-    $routes->fallbacks('InflectedRoute');
-});
