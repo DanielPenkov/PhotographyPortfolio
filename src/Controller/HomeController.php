5b444b16fb0e8e25ef23179a94b7388af63e21a9
Daniel <penkov.daniel@gmail.com>
2017-08-20 14:14:16 +0200

    Test deploy

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -40,7 +40,6 @@ class HomeController extends AppController {
 			->groupBy('session.album.name')
 			->toArray();
 
-
 		$otherSessionsCollection = (new Collection($groupedCollectionPictures['']))
 			->groupBy('session.album.name')
 			->toArray();
